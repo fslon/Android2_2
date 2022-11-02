342428ad8e4314307994e2bb2a9604faed633279
fslon <fedordikyn@gmail.com>
2022-11-02 20:36:03 +0300

    Initial commit

---
+++ app/src/main/java/ru/geekbrains/android2_2/view/MainActivity.kt
@@ -0,0 +1,18 @@
+package ru.geekbrains.android2_2.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ru.geekbrains.android2_2.R
+
+class MainActivity : AppCompatActivity() { // 41415
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/ru/geekbrains/android2_2/viewModel/MainViewModel.kt
@@ -0,0 +1,7 @@
+package ru.geekbrains.android2_2.viewModel
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
