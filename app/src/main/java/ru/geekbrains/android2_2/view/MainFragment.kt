1e0b8d045aaf5df60487029f70fca64a438441da
fslon <fedordikyn@gmail.com>
2022-11-03 18:57:51 +0300

    something

--- app/src/main/java/ru/geekbrains/android2_2/view/MainFragment.kt
+++ app/src/main/java/ru/geekbrains/android2_2/view/MainFragment.kt
@@ -6,6 +6,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.lifecycle.Observer
 import ru.geekbrains.android2_2.R
 import ru.geekbrains.android2_2.viewModel.MainViewModel
 
@@ -27,7 +29,39 @@ class MainFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
-        // TODO: Use the ViewModel
+
+        val observer = Observer<Any> { renderData(it)}
+        viewModel.getData().observe(viewLifecycleOwner, observer)
+
+    }
+
+    private fun renderData(data: Any) {
+        Toast.makeText(context, "data", Toast.LENGTH_LONG).show()
     }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- app/src/main/java/ru/geekbrains/android2_2/viewModel/MainViewModel.kt
+++ app/src/main/java/ru/geekbrains/android2_2/viewModel/MainViewModel.kt
@@ -1,7 +1,23 @@
 package ru.geekbrains.android2_2.viewModel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import java.lang.Thread.sleep
 
-class MainViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class MainViewModel(private val liveDataToObserve : MutableLiveData<Any> = MutableLiveData()) : ViewModel() {
+
+    fun getData(): LiveData<Any>{
+        getDataFromLocalSource()
+        return liveDataToObserve
+    }
+
+    private fun getDataFromLocalSource() {
+        Thread{
+            sleep(1000)
+            liveDataToObserve.postValue(Any())
+        }.start()
+    }
+
+
 }
