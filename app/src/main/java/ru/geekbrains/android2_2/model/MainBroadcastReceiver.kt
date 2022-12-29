e48a26f10c345a797bc7d009d50b217c78560374
fslon <fedordikyn@gmail.com>
2022-12-29 20:57:25 +0300

    1. Подпишитесь на событие изменения связи (CONNECTIVITY_ACTION) и уведомляйте об этом пользователя.

---
+++ app/src/main/java/ru/geekbrains/android2_2/model/MainBroadcastReceiver.kt
@@ -0,0 +1,14 @@
+package ru.geekbrains.android2_2.model
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.widget.Toast
+
+class MainBroadcastReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context?, intent: Intent?) {
+        Toast.makeText(context, "Изменено состояние связи", Toast.LENGTH_SHORT).show()
+    }
+
+}
--- app/src/main/java/ru/geekbrains/android2_2/view/MainActivity.kt
+++ app/src/main/java/ru/geekbrains/android2_2/view/MainActivity.kt
@@ -1,11 +1,16 @@
 package ru.geekbrains.android2_2.view
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.IntentFilter
+import android.net.ConnectivityManager
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import ru.geekbrains.android2_2.R
+import ru.geekbrains.android2_2.model.MainBroadcastReceiver
 import ru.geekbrains.android2_2.view.main.MainFragment
 
 class MainActivity : AppCompatActivity() { // 41415
+
+    private val receiver = MainBroadcastReceiver()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,10 +20,15 @@ class MainActivity : AppCompatActivity() { // 41415
                 .replace(R.id.container, MainFragment.newInstance())
                 .commitNow()
         }
+
+        registerReceiver(receiver, IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION))
+
     }
 
-
-
+    override fun onDestroy() {
+        unregisterReceiver(receiver)
+        super.onDestroy()
+    }
 
 
 }
